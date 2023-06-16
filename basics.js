2429956bdcfe1ad5196c4897d8423bfb077e093d
bisiAlgoRuthM <bisibeauty@yahoo.com>
2023-06-15 21:38:34 -0500

    .

---
+++ basics.js
@@ -0,0 +1,33 @@
+var myName = "ruth"
+var myCity = "Lagos"
+console.log(`My name is ${myName} My favorite city is ${myCity}.`);
+let newVariable = 'Playing around with typeof.';
+
+console.log(typeof newVariable)
+
+fahrenheit = Math.floor(fahrenheit);
+// my age
+var myAge = 20
+
+let earlyYears = 2
+earlyYears *= 10.5;
+laterYears = myAge - 2
+
+let myAgeInDogYears = earlyYears + laterYears
+var myName = "ruth".toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog`)
+var greeting = function() {
+    console.log('Hello World!');  
+  };
+  //With ES6 arrow functions, we can transform the expression above into:
+  
+  const greeting = () => console.log('Hello World'); 
+
+  let hungerLevel = 7
+
+if(hungerLevel > 7){
+  console.log('Time to eat!')
+}else{
+  console.log('We can eat later!')
+}
