16813f2cbc4e27dab577638539e4b4b36779c09c
bisiAlgoRuthM <bisibeauty@yahoo.com>
2023-09-21 11:27:43 -0500

    NEW FILE

---
+++ dogyears.js
@@ -0,0 +1,21 @@
+
+
+/*This program calculates a humans age dogyears given their current age*/
+
+var myAge = 26
+// current age
+
+let earlyYears = 2
+//early years 
+earlyYears *= 10.5;
+
+laterYears = myAge - 2 
+laterYears *= 4
+//later years
+
+let myAgeInDogYears = earlyYears + laterYears
+// dog years
+
+var myName = "Ruth".toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
