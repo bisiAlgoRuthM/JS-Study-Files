7b012468b820d8f2514801c2212ae76c8cc6a0bd
bisiAlgoRuthM <bisibeauty@yahoo.com>
2023-10-05 00:17:49 -0500

    ..

--- raceagesch.js
+++ raceagesch.js
@@ -20,3 +20,9 @@ if (runnersAge > 18) {
 
 
 
+const city = 'New York City'
+const logCitySkyline = () => {
+  let skyscraper = 'Empire State Building';
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+}
+console.log(logCitySkyline())
