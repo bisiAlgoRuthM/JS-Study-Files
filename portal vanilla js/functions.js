9ff8ca6dac288d54d9c655afe64e1691dce05b27
bisiAlgoRuthM <bisibeauty@yahoo.com>
2023-11-08 02:50:16 -0600

    updates

--- portal vanilla js/functions.js
+++ portal vanilla js/functions.js
@@ -11,3 +11,51 @@ function Student(name, age, matric_number, ){
 const studentInstance = new Student('John', 20, 'A1-11')
 console.log(Object.values(studentInstance))
 console.log(studentInstance.welcome_message())
+import React, { useState } from 'react';
+import { Button, Text, View } from 'react-native';
+
+const App = () => {
+  const [pressedCount, setPressedCount] = useState(0);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <Text style={{ margin: 16 }}>
+        {pressedCount > 0
+          ? `The button was pressed ${pressedCount} times!`
+          : 'The button isn\'t pressed yet'
+        }
+      </Text>
+      <Button
+        title='Press me'
+        onPress={() => setPressedCount(pressedCount + 1)}
+      />
+    </View>
+  );
+};
+
+export default App;
+
+import React, { useState } from 'react';
+import { Button, Text, View } from 'react-native';
+
+const App = () => {
+  const [pressedCount, setPressedCount] = useState(0);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <Text style={{ margin: 16 }}>
+        {pressedCount > 0
+          ? `The button was pressed ${pressedCount} times!`
+          : 'The button isn\'t pressed yet'
+        }
+      </Text>
+      <Button
+        title='Press me'
+        onPress={() => setPressedCount(pressedCount + 1)}
+        disabled = {pressedCount >= 3}
+      />
+    </View>
+  );
+};
+
+export default App;
